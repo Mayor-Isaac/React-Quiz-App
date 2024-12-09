9470057628686afdafd8af03b04373aa7ea40230
Mayor-Isaac <ogunyilekaferanmi@gmail.com>
2024-12-09 20:32:19 +0100

    Challenge Completed

--- src/Challenge/Challenge.js
+++ src/Challenge/Challenge.js
@@ -9,21 +9,28 @@ export default function Challenge() {
   };
 
   function reducer(state, action) {
+    if (state.disabled && action.type !== "openAcc") return state;
     if (action.type === "deposit")
-      return { ...state, balance: state.balance + 150 };
+      return { ...state, balance: state.balance + action.payload };
     if (action.type === "withdraw")
-      return { ...state, balance: state.balance - 50 };
-    if (action.type === "loan")
-      return {
-        ...state,
-        balance: state.balance + 5000,
-        loan: state.loan + 5000,
-      };
+      return { ...state, balance: state.balance - action.payload };
+    if (action.type === "loan") {
+      if (state.loan > 0) return state;
+      else
+        return {
+          ...state,
+          balance: state.balance + action.payload,
+          loan: state.loan + action.payload,
+        };
+    }
     if (action.type === "payLoan")
       return { ...state, balance: state.balance - state.loan, loan: 0 };
     if (action.type === "openAcc")
       return { ...state, disabled: false, balance: 500 };
-    if (action.type === "closeAcc") return { ...initialState };
+    if (action.type === "closeAcc") {
+      if (state.balance === 0 || state.loan === 0) return { ...initialState };
+      else return state;
+    }
   }
 
   const [state, dispatch] = useReducer(reducer, initialState);
@@ -34,14 +41,17 @@ export default function Challenge() {
       <h3>Balance : {balance}</h3>
       <h3>Loan : {loan}</h3>
       <div>
-        <button onClick={() => dispatch({ type: "openAcc" })}>
+        <button
+          disabled={!disabled}
+          onClick={() => dispatch({ type: "openAcc" })}
+        >
           Open Account
         </button>
       </div>
       <div>
         <button
           disabled={disabled}
-          onClick={() => dispatch({ type: "deposit" })}
+          onClick={() => dispatch({ type: "deposit", payload: 150 })}
         >
           Deposit 150
         </button>
@@ -49,15 +59,15 @@ export default function Challenge() {
       <div>
         <button
           disabled={disabled}
-          onClick={() => dispatch({ type: "withdraw" })}
+          onClick={() => dispatch({ type: "withdraw", payload: 50 })}
         >
           Withdraw 50
         </button>
       </div>
       <div>
         <button
-          disabled={disabled || loan > 0}
-          onClick={() => dispatch({ type: "loan" })}
+          disabled={disabled}
+          onClick={() => dispatch({ type: "loan", payload: 5000 })}
         >
           Request a loan of 5000
         </button>
